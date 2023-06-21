77f69a4675d1f90ae1dec08896298d825d2d8d86
AntonHran <anton.hranovskyi@gmail.com>
2023-06-22 00:47:29 +0300

    greeting was added

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import asyncio
 main_ = {1: address_book_main, 2: clean_folder_main, 3: game_main, }
 
 if __name__ == '__main__':
+    print('''Welcome to the CUI personal assistant.
+    Here I have my Main menu. It consists of three blocks: 
+    1. addressbook with your contacts, 
+    2. cleaning block where I can sort your files and 
+    3. gaming block for a bit of entertainment.\n
+    To choice the block, just enter its number 1, 2 or 3 in the offered field.\n
+    To close the personal assistant, enter 0\n''')
     while True:
         choice = int(input('Please make your choice: '))
         if choice == 0:
