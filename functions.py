9762c156a24f6d996d7ff80de79491bfc961cdd5
AntonHran <anton.hranovskyi@gmail.com>
2023-06-24 23:06:20 +0300

    output_, commands_addressbook, TerminalView, TerminalPrint were imported from classes.py; in all functions docstrings were deleted, instead of these variables with command descriptions were added, then they were added to instance of class TerminalView; function 'show_field' was added; to all functions arguments (contact and/or output and/or commands) were added to not use global variables directly in functions; dictionary 'methods' were changed: each value became a named tuple Function; function 'make_function' was added to define what exactly function should be used and to build that function; function 'address_book_main' was rewritten according to changes.

--- functions.py
+++ functions.py
@@ -1,15 +1,16 @@
 import re
-from classes import AddressBook, Record, Name, contacts
+from classes import AddressBook, Record, Name, contacts, output_, commands_addressbook, TerminalView, TerminalPrint
 import pickle
-from typing import Callable
+# from typing import Callable, Tuple
 from functools import wraps
+import collections
 
 
 def input_error(func):
     @wraps(func)
-    def inner_func(args):
+    def inner_func(*args):
         try:
-            return func(args) if args else func()
+            return func(*args) if args else func()
         except Exception as exc:
             print(exc)
         except (KeyError, ValueError, IndexError, TypeError) as error:
@@ -30,187 +31,255 @@ def read_info_from_file() -> AddressBook:
 
 
 @input_error
-def add_contact(name: str) -> None:
-    """
+def add_contact(name: str, contact_book: AddressBook) -> None:
+    name: Name = Name(name)
+    record: Record = Record(name)
+    contact_book.add_record(record)
+    write_info_from_class(contact_book)
+
+
+add_contact_comm = """
     A name of a contact.
     The field name cannot be empty.
     To create a new contact, type: add contact <name of a new contact>"""
-    name: Name = Name(name)
-    record: Record = Record(name)
-    contacts.add_record(record)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(add_contact_comm)
 
 
 @input_error
-def delete_contact(name: str) -> None:
-    """
+def delete_contact(name: str, contact: AddressBook) -> None:
+    contact.delete_record(name)
+    write_info_from_class(contact)
+
+
+delete_contact_comm = """
     To delete some contact, type: delete contact <name of a contact>"""
-    contacts.delete_record(name)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(delete_contact_comm)
 
 
 @input_error
-def search(keyword: str) -> None:
-    """
-    To search a contact by a name or a phone or an email a date of birth or a status either a note,
+def search(keyword: str, contact: AddressBook, output: TerminalPrint) -> None:
+    result: list = contact.search_by_keyword(keyword)
+    [res.display(output) for res in result]
+
+
+search_comm = """
+    To search a contact by a name or a phone or an email a date of birth or a status or other fields,
     using a full value of the field name or only a part of it, type: search <keyword>"""
-    result: list = contacts.search_by_keyword(keyword)
-    [print(res) for res in result]
+commands_addressbook.add_command(search_comm)
 
 
-def show_contacts() -> None:
-    """
-    To show all notices of an address book, type: show all"""
+def show_contacts(output: TerminalPrint, pages: int = 2) -> None:
     contacts_download = read_info_from_file()
-    for record in contacts_download.iterator():
-        print('\n'.join(record))
+    for record in contacts_download.iterator(pages):
+        [field.display(output) for field in record]
         input('Press "Enter": ')
 
 
-def instructions() -> None:
+show_contacts_comm = """
+    To show all notices of an address book, type: show all"""
+commands_addressbook.add_command(show_contacts_comm)
+
+
+def show_commands(commands: TerminalView) -> None:
     print('\n\tGeneral commands for all written contacts:\n'
           '\tNames, phone numbers, emails and other parameters have to be written without brackets <...>')
-    for command in methods.values():
-        if command.__doc__:
-            print(command.__doc__)
+    for command in commands.display_commands():
+        print(command)
+
+
+show_commands_comm = """
+    To show a list of all commands with instructions, type: help"""
+commands_addressbook.add_command(show_commands_comm)
 
 
 @input_error
-def change_name(name: str) -> None:
-    """
-    To change the name of some contact, type: change name <name of contact>"""
-    name: str = contacts.search_by_name(name)
-    contact_data: Record = contacts.data[name]
-    contacts.delete_record(name)
+def change_name(name: str, contact: AddressBook) -> None:
+    name: str = contact.search_by_name(name)
+    contact_data: Record = contact.data[name]
+    contact.delete_record(name)
     new_name: str = input('Please enter a new name for the contact: ')
     contact_data.name.set_value(new_name)
-    contacts.add_record(contact_data)
-    write_info_from_class(contacts)
+    contact.add_record(contact_data)
+    write_info_from_class(contact)
+
+
+change_name_comm = """
+    To change the name of some contact, type: change name <name of contact>"""
+commands_addressbook.add_command(change_name_comm)
 
 
 @input_error
-def add_phone_number(name: str) -> None:
-    """
+def add_phone_number(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
+    number: str = input('Please enter a phone number according to a phone pattern: +<code of a country>XXXXXXXXX '
+                        'or <operator code>XXXXXXX: ')
+    contact.data[name].phone.set_value(number)
+    write_info_from_class(contact)
+
+
+add_phone_number_comm = """
     A phone number of a contact.
     All phone numbers should be added according to a phone pattern: +<code of a country>XXXXXXXXX or
     <operator code>XXXXXXX
     All phone numbers are saved according to the pattern: +<code of a country>(XX)XXXXXXX
 
     To add a new phone number to some contact, type: add phone <name of a contact>"""
-    name = contacts.search_by_name(name)
-    number: str = input('Please enter a phone number according to a phone pattern: +<code of a country>XXXXXXXXX '
-                        'or <operator code>XXXXXXX: ')
-    contacts.data[name].phone.set_value(number)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(add_phone_number_comm)
 
 
 @input_error
-def delete_phone_number(name: str) -> None:
-    """
-    To change a phone number of some contact, type: change phone <name of a contact>"""
-    name = contacts.search_by_name(name)
+def delete_phone_number(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
     number: str = input('Please enter a number to delete: ')
-    contacts.data[name].phone.delete_phone_number(number)
-    write_info_from_class(contacts)
+    contact.data[name].phone.delete_phone_number(number)
+    write_info_from_class(contact)
+
+
+delete_phone_number_comm = """
+    To change a phone number of some contact, type: change phone <name of a contact>"""
+commands_addressbook.add_command(delete_phone_number_comm)
 
 
 @input_error
-def change_phone_number(name: str) -> None:
-    """
-    To change a phone number of some contact, type: change phone <name of a contact>"""
-    name = contacts.search_by_name(name)
+def change_phone_number(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
     phone_num: str = input('Enter <an old phone number>-<new phone number> for the contact: ')
     old, new = phone_num.strip().split('-')
-    contacts.data[name].phone.delete_phone_number(old)
-    contacts.data[name].phone.set_value(new)
-    write_info_from_class(contacts)
+    contact.data[name].phone.delete_phone_number(old)
+    contact.data[name].phone.set_value(new)
+    write_info_from_class(contact)
+
+
+change_phone_number_comm = """
+    To change a phone number of some contact, type: change phone <name of a contact>"""
+commands_addressbook.add_command(change_phone_number_comm)
 
 
 @input_error
-def change_email(name: str) -> None:
-    """
+def change_email(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
+    email: str = input('Please enter an email for the contact: ')
+    contact.data[name].email.set_value(email)
+    write_info_from_class(contact)
+
+
+change_email_comm = """
     An email of a contact.
     All emails should be written according to a valid format of your email type.
     To change or add (if this field is empty), an email of some contact, type: change email <name of a contact>"""
-    name = contacts.search_by_name(name)
-    email: str = input('Please enter an email for the contact: ')
-    contacts.data[name].email.set_value(email)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(change_email_comm)
 
 
 @input_error
-def change_birthdate(name: str) -> None:
-    """
+def change_birthdate(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
+    birthdate: str = input('Please enter a date of birth for the contact according to pattern YYYY-MM-DD: ')
+    contact.data[name].bd.set_value(birthdate)
+    write_info_from_class(contact)
+
+
+change_birthdate_comm = """
     A birthday of a contact.
     All dates of the birthday should be written according to the pattern: YYYY-MM-DD
     To change/add (if this field is empty) a birthdate of some contact, type:
     change bd <name of a contact>"""
-    name = contacts.search_by_name(name)
-    birthdate: str = input('Please enter a date of birth for the contact according to pattern YYYY-MM-DD: ')
-    contacts.data[name].bd.set_value(birthdate)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(change_birthdate_comm)
 
 
 @input_error
-def days_to_birthday(name: str) -> None:
-    """
+def days_to_birthday(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
+    print(f'Number days to birthday for {name} is {contact.data[name].bd.days_to_birthday()} days.')
+
+
+days_to_birthday_comm = """
     To show how many days left to someone's birthday, type: days to bd <name of a contact>"""
-    name = contacts.search_by_name(name)
-    print(f'Number days to birthday for {name} is {contacts.data[name].bd.days_to_birthday()} days.')
+commands_addressbook.add_command(days_to_birthday_comm)
 
 
 @input_error
-def change_status(name: str) -> None:
-    """
+def change_status(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
+    status: str = input('Please enter one of statuses to this contact: '
+                        'Friend, Family, Co-Worker, Special. Or leave it empty: ')
+    contact.data[name].status.set_value(status)
+    write_info_from_class(contact)
+
+
+change_status_comm = """
     A status of a contact.
     You can add one of the following statuses to your contact: Friend, Family, Co-Worker, Special.
     Or it can be empty.
     To add/change a status of some contact, type: change status <name of a contact>"""
-    name = contacts.search_by_name(name)
-    status: str = input('Please enter one of statuses to this contact: '
-                        'Friend, Family, Co-Worker, Special. Or leave it empty: ')
-    contacts.data[name].status.set_value(status)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(change_status_comm)
 
 
 @input_error
-def add_note(name: str) -> None:
-    """
+def add_note(name: str, contact: AddressBook) -> None:
+    name = contact.search_by_name(name)
+    note: str = input('Please enter a note for the contact: ')
+    contact.data[name].note.set_value(note)
+    write_info_from_class(contact)
+
+
+add_note_comm = """
     A note of a contact.
     To add/change notes of some contact, type: change note <name of a contact>
     Or it can be empty."""
-    name = contacts.search_by_name(name)
-    note: str = input('Please enter a note for the contact: ')
-    contacts.data[name].note.set_value(note)
-    write_info_from_class(contacts)
+commands_addressbook.add_command(add_note_comm)
 
 
-def greeting(object_: AddressBook):
-
-    print('\n\tNow you are in your personal addressbook.\n'
-          '\tI can help you with adding, changing, showing and storing all contacts and data connected with them.')
-    try:
-        object_.data.clear()
-        [contacts.add_record(value) for value in read_info_from_file().values()]
-        # contacts = read_info_from_file()
-    except (FileExistsError, FileNotFoundError):
-        print('There are not records yet. Your addressbook is empty.')
+@input_error
+def show_field(name: str, contact: AddressBook, output: TerminalView) -> None:
+    name_check = contact.search_by_name(name)
+    field: str = input('Please type a field you want to see for this contact: ')
+    contact.data[name_check].display_field(field, output)
 
 
-def farewell() -> None:
-    """
-    To exit the addressbook and come back to the main menu, type: back
-    """
-    write_info_from_class(contacts)
+show_field_comm = """
+    To show the necessary field of some contact, type: show field <name of a contact>"""
+commands_addressbook.add_command(show_field_comm)
+
+
+def farewell(contact: AddressBook) -> None:
+    write_info_from_class(contact)
     print('All changes saved successfully.\n'
           '\nYou returned to the main Menu.')
 
 
-methods = {'add contact': add_contact, 'delete contact': delete_contact, 'search': search,
-           'show all': show_contacts, 'change name': change_name, 'add phone': add_phone_number,
-           'change phone': change_phone_number, 'delete phone': delete_phone_number, 'change email': change_email,
-           'change bd': change_birthdate, 'days to bd': days_to_birthday, 'change status': change_status,
-           'add note': add_note, 'help': instructions, }
+farewell_comm = """
+    To exit the addressbook and come back to the main menu, type: back"""
+commands_addressbook.add_command(farewell_comm)
+
+
+def greeting(contact: AddressBook):
+    print('\n\tNow you are in your personal addressbook.\n'
+          '\tI can help you with adding, changing, showing and storing all contacts and data connected with them.')
+    try:
+        contact.data.clear()
+        [contact.add_record(value) for value in read_info_from_file().values()]
+    except (FileExistsError, FileNotFoundError):
+        print('There are not records yet. Your addressbook is empty.')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+Function = collections.namedtuple('Function', ['function', 'records', 'terminal_output', 'terminal_commands'])
+
+methods = {'add contact': Function(add_contact, AddressBook, '', ''),
+           'delete contact': Function(delete_contact, AddressBook, '', ''),
+           'search': Function(search, AddressBook, TerminalPrint, ''),
+           'show all': Function(show_contacts, '', TerminalPrint, ''),
+           'change name': Function(change_name, AddressBook, '', ''),
+           'add phone': Function(add_phone_number, AddressBook, '', ''),
+           'change phone': Function(change_phone_number, AddressBook, '', ''),
+           'delete phone': Function(delete_phone_number, AddressBook, '', ''),
+           'change email': Function(change_email, AddressBook, '', ''),
+           'change bd': Function(change_birthdate, AddressBook, '', ''),
+           'days to bd': Function(days_to_birthday, AddressBook, '', ''),
+           'change status': Function(change_status, AddressBook, '', ''),
+           'add note': Function(add_note, AddressBook, '', ''),
+           'help': Function(show_commands, '', '', TerminalView),
+           'show field': Function(show_field, AddressBook, TerminalPrint, ''), }
 
 
 @input_error
@@ -220,21 +289,48 @@ def command_parser(command: str) -> tuple:
             return func, re.sub(func, '', command, flags=re.I).strip()
 
 
-def handler(function_name: str) -> type[Callable]:
+def handler(function_name: str) -> collections.namedtuple:  # Tuple[Callable, None | AddressBook, None | TerminalView]:
     return methods[function_name]
+
+
+def make_function(text: str) -> None:
+    try:
+        command, argument = command_parser(text)
+        print(command, '|', argument)
+        func: collections.namedtuple = handler(command)
+        print(func)
+
+        if argument and func.records and func.terminal_output:
+            print(1)
+            func.function(argument, contacts, output_)
+        elif argument and func.records and not func.terminal_output:
+            print(2)
+            func.function(argument, contacts)
+        elif argument and func.terminal_output and not func.records:
+            print(3)
+            func.function(argument, output_)
+        elif not argument and func.terminal_output and not func.records:
+            print(4)
+            func.function(output_)
+        elif not argument and func.terminal_commands and not func.records:
+            print(5)
+            func.function(commands_addressbook)
+
+    except (TypeError, KeyError) as e:
+        print(e)
+        print('I do not understand what you want to do. Please look at the commands.')
 
 
 def address_book_main():
     greeting(contacts)
-    instructions()
+    show_commands(commands_addressbook)
     while True:
         text: str = input('\nEnter your command: ')
         if text == 'back':
-            farewell()
+            farewell(contacts)
             break
-        try:
-            command, argument = command_parser(text)
-            func = handler(command)
-            func(argument) if argument else func()
-        except (TypeError, KeyError):
-            print('I do not understand what you want to do. Please look at the commands.')
+        make_function(text)
+
+
+'''add_contact('kjh', contacts)
+add_note('kjg', contacts)'''
