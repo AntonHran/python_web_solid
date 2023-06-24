92d876eaea6846f6617b90df1139c12e32a768f8
AntonHran <anton.hranovskyi@gmail.com>
2023-06-24 23:13:11 +0300

    function 'main_instructions' was changed; instance of class TerminalView was added to write, show and store instructions of the main menu; dict 'main_' was renamed as 'main_functions'.

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from functions import address_book_main
 from cleaner_functions import clean_folder_main
 from game import game_main
 import asyncio
+from classes import TerminalView
 
-
-def main_instructions() -> None:
-    print('''\n\t1. addressbook with your contacts, 
+main_commands = TerminalView()
+main_commands_comm = '''\n\t1. addressbook with your contacts, 
     2. cleaning block where I can sort your files and thus clean your folder,
     3. gaming block for a bit of entertainment.\n
     To choice the block, just enter its number 1, 2 or 3 in the offered field.\n
     To see instructions of main menu, type 4.
-    To close the personal assistant, enter 0''')
+    To close the personal assistant, enter 0'''
+main_commands.add_command(main_commands_comm)
 
 
-main_ = {1: address_book_main, 2: clean_folder_main, 3: game_main, 4: main_instructions}
+def main_instructions() -> None:
+    for command in main_commands.display_commands():
+        print(command)
+
+
+main_functions = {1: address_book_main, 2: clean_folder_main, 3: game_main, 4: main_instructions}
 
 
 if __name__ == '__main__':
@@ -25,4 +31,4 @@ if __name__ == '__main__':
         if choice == 0:
             print('\nGood bye)')
             break
-        main_[choice]() if choice != 2 else asyncio.run(main_[choice]())
+        main_functions[choice]() if choice != 2 else asyncio.run(main_functions[choice]())
